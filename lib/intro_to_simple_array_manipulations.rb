cc5c9f125fe04047383ab4749dcc5aa559fb8d07
Daniel Succar <danielsuccar@icloud.com>
2020-01-16 19:47:32 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+def using_push(colors_in_the_rainbow, next_color)
+  updated_array = colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(bouroughs_in_nyc,new_neighborhood)
+  using_unshift = bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(continents)
+  using_pop = continents.pop
+end
+
+def pop_with_args(dog_breeds)
+  pop_with_args = dog_breeds.pop(2)
+end
+
+def using_shift(my_favorite_cities)
+  using_shift = my_favorite_cities.shift
+end
+
+def shift_with_args(ice_cream_brands)
+  shift_with_args = ice_cream_brands.shift(2)
+end
+
+def using_concat (my_favorite_things, more_favs)
+  using_concat = my_favorite_things.concat(more_favs)
+end
+
+def using_insert (list_of_programming_languages,another_language)
+  using_insert = list_of_programming_languages.insert(4, another_language)
+
+end
+
+def using_uniq(haircuts)
+  using_uniq = haircuts.uniq
+end
+
+def using_flatten(instruments)
+  using_flatten = instruments.flatten
+end
+
+def using_delete(instructors,no_offense_steven)
+  using_delete = instructors.delete(no_offense_steven)
+end
+
+def using_delete_at(famous_robots,n)
+  using_delete_at = famous_robots.delete_at(n)
+
+end
